39681832f2b0bd2c4e32c7f898b5a4ca650dbb8e
codingc1 <codingc@hanmail.net>
2023-12-29 12:57:49 +0900

    env_err

--- src/api/app-setting.ts
+++ src/api/app-setting.ts
@@ -8,20 +8,19 @@ export const REFESH_TOKEN_TIMER = 1000*60*3; //리프레쉬 시간
 // export const BaseHangUrlName = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development')?'http://localhost:3000':'https://hangbal.net' 
 
 export const isProduction =() => {
-    if ( process.env.NODE_ENV === 'prod') { //development
-        return true
-    } else {
+    console.log(process.env.NODE_ENV, 'env.NODE_ENV??')
+    if ( process.env.NODE_ENV === 'development') { //
         return false
+    } else {
+        return true
     }
 }
 export const serverAddress =()=>{
     // return 'https://ec2.hangbal.net'
     // return 'http://3.37.198.102:2021';
     if (isProduction()===false) {
-        console.log('development')
         return "http://localhost:2021";
     } else {
-        console.log('development')
         return 'https://ec2.hangbal.net'
     }
 
