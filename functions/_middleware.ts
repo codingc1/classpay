58bce90ec752c223c0319c852c2cdcf84a2c81a8
codingc1 <codingc@hanmail.net>
2024-01-07 14:39:50 +0900

    fex_es5

--- functions/_middleware.ts
+++ functions/_middleware.ts
@@ -1,22 +1,21 @@
-
-// Respond to OPTIONS method //PagesFunction
-export const onRequestOptions: any = async () => {
+function onRequestOptions() {
     return new Response(null, {
-      status: 204,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': '*',
-        'Access-Control-Allow-Methods': 'GET, OPTIONS',
-        'Access-Control-Max-Age': '86400',
-        'Access-Control-Expose-Headers':'*',
-      },
+        status: 204,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Headers': '*',
+            'Access-Control-Allow-Methods': 'GET, OPTIONS',
+            'Access-Control-Max-Age': '86400',
+            'Access-Control-Expose-Headers': '*',
+        },
     });
-  };
-  
-  // Set CORS to all /api responses
-  export const onRequest: any = async (context) => {
-    const response = await context.next();
-    response.headers.set('Access-Control-Allow-Origin', '*');
-    response.headers.set('Access-Control-Max-Age', '86400');
-    return response;
-  };
+}
+
+// Set CORS to all /api responses
+function onRequest(context) {
+    return context.next().then(function (response) {
+        response.headers.set('Access-Control-Allow-Origin', '*');
+        response.headers.set('Access-Control-Max-Age', '86400');
+        return response;
+    });
+}
