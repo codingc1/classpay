850a51236c8cd6489f0644459f6f2e0262019646
codingc1 <codingc@hanmail.net>
2024-03-26 19:55:19 +0900

    Fix_tostudy_alram

--- src/page/class-pay/zhome-classpay-app/today-study-content.tsx
+++ src/page/class-pay/zhome-classpay-app/today-study-content.tsx
@@ -1,50 +1,80 @@
-import { useReactiveVar } from "@apollo/client";
 import { useCpPayUserList } from "../../../hooks/cp-pay/cp-pay-user/useCpPayUserList";
 import { grayBtnCss } from "../../../components/button/tailwind-btn/gray-button";
 import { useNavigate } from "react-router-dom";
 import { CP_PAY_MEMBER_ROUTE_NAME } from "../../../routers/route-name-constants";
-import { useMe } from "../../../hooks/user/useMe";
-
+import { useEffect, useState } from "react";
+import { CP_INSTITUTION_CENTERBANK_ROUTE_NAME } from "../../../routers/contains/ecomomy";
 
 
  
 export const TodayStudyContent = () => {
   let navigate = useNavigate()
   const{data} = useCpPayUserList() 
-  const {data:meData} = useMe()
+  const [alramStatus, setAlramStatus] = useState('')
+  // const {data:meData} = useMe()
   // const institution = useReactiveVar(cpInstitutionVar).institution;
   const studentList = data && data.cp_PayUserLists?data.cp_PayUserLists:[]
 //학생을 등록해 주세요
 //화폐를 발행해 주세요
 //처음 하는 교사에게 해야할 것 안내
-
+useEffect(()=>{
+  if(studentList.length <= 1){
+    setAlramStatus('NO_STUDENT')
+  }else if(isNoMoney()){
+    setAlramStatus('NO_MONEY')
+  }else{
+    setAlramStatus('')
+  }
+},[studentList])
 //cp_institution home에서 모든 기관을 사용 (넘복잡) => 각 기관별로 이동..
 const isNoMoney = ()=>{
   //money가 0이 아닌 학생 찾기
-  if(!data || !meData) return false
+  if(!data ) return false
+  // if(meData.cp_me.money !== 0) return false
   const students = data.cp_PayUserLists
   for(let i=0; i<students.length; i++){
-    if(students[i].money !== 0) return false
+    if(students[i].money !== 0) {
+      return false
+    }
   }
-  if(meData.cp_me.money !== 0) return false
   return true
 }
-const firstUserView = ()=>{
-  if(studentList.length ===0){
+// const firstUserView = ()=>{
+//   if(studentList.length <= 1){
+//     return (
+//     <div className="flex px-2 items-center">
+//       <div>학생 등록하기</div>
+//       <div className={grayBtnCss({})} onClick={()=>navigate(CP_PAY_MEMBER_ROUTE_NAME)}>이동</div>
+//     </div>
+//     )
+//   }else if(isNoMoney()){
+//     <div className="flex px-2 items-center">
+//       <div>화폐 발행하기</div>
+//       <div className={grayBtnCss({})} onClick={()=>navigate(CP_PAY_MEMBER_ROUTE_NAME)}>이동</div>
+//     </div>
+//   }else{
+//     return <div></div>
+//   }
+// }
+
+const alramView = ()=>{
+  if(alramStatus === 'NO_STUDENT'){
     return (
-    <div className="flex px-2 items-center">
-      <div>학생 등록하기</div>
+      <div className="flex px-2 items-center">
+      <div>학생을 등록해 주세요</div>
       <div className={grayBtnCss({})} onClick={()=>navigate(CP_PAY_MEMBER_ROUTE_NAME)}>이동</div>
     </div>
     )
-  }else if(isNoMoney()){
+}
+if(alramStatus === 'NO_MONEY'){ 
+  return (
     <div className="flex px-2 items-center">
       <div>화폐 발행하기</div>
-      <div className={grayBtnCss({})} onClick={()=>navigate(CP_PAY_MEMBER_ROUTE_NAME)}>이동</div>
+      <div className={grayBtnCss({})} onClick={()=>navigate(CP_INSTITUTION_CENTERBANK_ROUTE_NAME)}>이동</div>
     </div>
-  }else{
-    return <div></div>
-  }
+    )
+}
+return <div></div>
 }
 
 
@@ -53,7 +83,8 @@ return(
   <div className="w-full h-20 p-3 flex flex-col   bg-white rounded-lg">
     <div className="w-full px-2 font-bold">공부할 내용</div>
     <div>
-      {firstUserView()}
+      {/* {firstUserView()} */}
+      {alramView( )}
     </div>
   </div>
 </article>
